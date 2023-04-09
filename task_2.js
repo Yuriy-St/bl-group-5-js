ee6c7e188414de28f08e077141f306916c99435e
KolBa228 <technotrashinfo@gmail.com>
2023-04-09 14:27:57 +0100

    up3

--- task_2.js
+++ task_2.js
@@ -2,3 +2,9 @@
 // який буде виводити в консоль всі парні числа від min до max
 const max = 50;
 const min = 0;
+
+for (let i = min; i <= max; i += 1) {
+    if(i % 2 === 0 && i !== 0) {
+        console.log(i)
+    }
+}
