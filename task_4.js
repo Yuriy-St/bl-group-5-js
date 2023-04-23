4ad2d01d2c201d2c7c4bf1bc6b37e990f048ac81
O-Kareta <aleksandr.kareta@gmail.com>
2023-04-23 15:45:46 +0300

    task4

--- task_4.js
+++ task_4.js
@@ -2,15 +2,22 @@
 // Name: Peter. Surname: Parker. Age: 22. Position: spider-man.
 
 const user = {
-    name: 'Peter',
-    surname: 'Parker',
-    age: 22,
-    position: 'spider-man'
+  name: 'Peter',
+  surname: 'Parker',
+  age: 22,
+  position: 'spider-man',
+  getinfo: function () {
+    return `Name: ${ this.name }. Surname: ${ this.surname }. Age: ${ this.age }. Position: ${this.position}.`
+  }
 }
+
+console.log(user.getinfo());
 
 const user2 = {
   name: "Batman",
   surname: "Hz",
   age: 36,
   position: "batman",
+
+
 };
