0130e1d1e543b98dd3db08bd3b832bfa96a18904
Volodymyr Kozak <vkozak@reenbit.com>
2023-04-23 14:06:03 +0300

    upload tasks for lesson 2

---
+++ task_4.js
@@ -0,0 +1,16 @@
+// Додати метод getinfo в об'єкт user, який повертатиме інфу про користувача у форматі
+// Name: Peter. Surname: Parker. Age: 22. Position: spider-man.
+
+const user = {
+    name: 'Peter',
+    surname: 'Parker',
+    age: 22,
+    position: 'spider-man'
+}
+
+const user2 = {
+  name: "Batman",
+  surname: "Hz",
+  age: 36,
+  position: "batman",
+};
