5ef25e92da795ee939b62346c278228d0740321e
unknown <jack22toplolkek@gmail.com>
2023-05-07 16:10:08 +0300

    task2

--- task_1.js
+++ task_1.js
@@ -12,22 +12,22 @@ import users from "./data.js";
 // Отримати загальну суму балансу (поле balance) всіх користувачів.
 // console.log(calculateTotalBalance(users)); // 20916
 
-function getUserNames(usersData) {
-  return usersData.map((user) => user.name);
-}
+// function getUserNames(usersData) {
+//   return usersData.map((user) => user.name);
+// }
 
-console.log(getUserNames(users));
+// console.log(getUserNames(users));
 
-function getUsersWithEyeColor(users, color) {
-  return users.filter((user) => color === user.eyeColor);
-}
+// function getUsersWithEyeColor(users, color) {
+//   return users.filter((user) => color === user.eyeColor);
+// }
 
-console.log(getUsersWithEyeColor(users, "blue"));
+// console.log(getUsersWithEyeColor(users, "blue"));
 
-function calculateTotalBalance(users) {
-  return users.reduce((acc, user) => {
-    return user.balance + acc;
-  }, 0);
-}
+// function calculateTotalBalance(users) {
+//   return users.reduce((acc, user) => {
+//     return user.balance + acc;
+//   }, 0);
+// }
 
-console.log(calculateTotalBalance(users));
+// console.log(calculateTotalBalance(users));
--- task_2.js
+++ task_2.js
@@ -2,3 +2,36 @@
 //з ​​властивостями login email
 //Оголоси приватні властивості #login #email,
 //доступ до яких зроби через геттер та сеттер login email
+
+class Client {
+  #login;
+  #email;
+
+  constructor({ login, email }) {
+    this.#login = login;
+    this.#email = email;
+  }
+
+  get email() {
+    return this.#email;
+  }
+
+  set email(newEmail) {
+    this.#email = newEmail;
+  }
+
+  get login() {
+    return this.#login;
+  }
+
+  set login(newLogin) {
+    this.#login = newLogin;
+  }
+}
+
+const newClient = new Client({
+  email: "usertop@mail.com",
+  login: "topuseR",
+});
+
+console.log(newClient);
