0cf59c1b2ce3f41c249167cc2dc9c935e8028c31
ElHoma <121969975+ElHoma@users.noreply.github.com>
2023-05-07 16:00:21 +0300

    task 1

--- task_1.js
+++ task_1.js
@@ -1,4 +1,4 @@
-import users from './data.js'
+import users from "./data.js";
 
 //1
 // Отримати масив імен всіх користувачів (поле name).
@@ -12,3 +12,22 @@ import users from './data.js'
 // Отримати загальну суму балансу (поле balance) всіх користувачів.
 // console.log(calculateTotalBalance(users)); // 20916
 
+function getUserNames(usersData) {
+  return usersData.map((user) => user.name);
+}
+
+console.log(getUserNames(users));
+
+function getUsersWithEyeColor(users, color) {
+  return users.filter((user) => color === user.eyeColor);
+}
+
+console.log(getUsersWithEyeColor(users, "blue"));
+
+function calculateTotalBalance(users) {
+  return users.reduce((acc, user) => {
+    return user.balance + acc;
+  }, 0);
+}
+
+console.log(calculateTotalBalance(users));
