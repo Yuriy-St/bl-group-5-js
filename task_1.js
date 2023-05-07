4b3c6502c292058f0a53593b55bda1067499fd9c
Volodymyr Kozak <vkozak@reenbit.com>
2023-05-07 15:39:11 +0300

    add tasks for lesson 3

---
+++ task_1.js
@@ -0,0 +1,14 @@
+import users from './data.js'
+
+//1
+// Отримати масив імен всіх користувачів (поле name).
+// console.log(getUserNames(usersData)) - [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+//2
+// Отримати масив об'єктів користувачів за кольором очей (поле eyeColor).
+// console.log(getUsersWithEyeColor(users, 'blue')); - // [об'єкт Moore Hensley, об'єкт Sharlene Bush, об'єкт Carey Barr]
+
+//3
+// Отримати загальну суму балансу (поле balance) всіх користувачів.
+// console.log(calculateTotalBalance(users)); // 20916
+
