83b1737a454d05926b88df8e5ca7a3edf575013b
Вячеслав <goldensoldat@gmail.com>
2023-05-07 16:45:43 +0300

    Task-3

--- task_3.js
+++ task_3.js
@@ -5,3 +5,38 @@
 //який буде повертати об'єкт із пріоритетами.
 //Додай методи addNote(note), removeNote(text)
 //updatePriority(text, newPriority)
+
+class Notes {
+    static Priority() {
+        return {
+            HIGH: 'high',
+            LOW: 'low',
+        }
+    }
+    constructor() {
+        this.items = [];
+
+    }
+    addNote(note) {
+        return this.items.push(note);
+    }
+    removeNote(text) {
+        this.items = this.items.filter(el => el.text !== text)
+    }
+    updatePriority(text, newPriority) {
+        const indexNote = this.items.findIndex(el => el.text === text)
+        this.items[indexNote].priority = newPriority
+    }
+}
+const notes = new Notes();
+notes.addNote({ text: 'hello', priority: Notes.Priority().LOW });
+notes.addNote({ text: 'WORLD', priority: Notes.Priority().HIGH });
+notes.removeNote("hello");
+notes.updatePriority('WORLD', Notes.Priority().LOW);
+console.log('notes: ', notes);
+
+
+const person = {
+    name: 'Jon',
+
+}
