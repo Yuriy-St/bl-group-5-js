b0085cc88286f4de11327e99ba972edf55e06aa9
Yuriy-St <yuriystaynov@gmail.com>
2023-06-04 15:23:00 +0300

    task 3

--- task_3.js
+++ task_3.js
@@ -3,3 +3,31 @@
 // Якщо значення не є числом, відхиляй проміс та повертай "error".
 // Якщо значення парне, вирішуй проміс та повертай "even" через 1 секунду.
 // Якщо значення не парне, вирішуй проміс та повертай "odd" через 2 секунди.
+
+const value = prompt('Введіть Ваше значення');
+
+function checkValue(value) {
+	return new Promise((resolve, reject) => {
+		if (isNaN(value)) {
+			reject('error');
+		}
+
+		if (value % 2 === 0) {
+			setTimeout(() => {
+				resolve('even')
+			}, 1000);
+		}
+
+		setTimeout(() => {
+			resolve('odd');
+		}, 2000)
+	})
+}
+
+checkValue(value)
+	.then(res => {
+		console.log(res);
+	})
+	.catch(e => {
+		console.error(e);
+})
