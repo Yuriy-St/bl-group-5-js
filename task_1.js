0fac48a95f2b21a189e749904e7b424eb187fce8
unknown <jack22toplolkek@gmail.com>
2023-06-04 14:52:55 +0300

    task1

--- task_1.js
+++ task_1.js
@@ -1,1 +1,7 @@
 // Створіть функцію, яка отримує функцію як аргумент і виконує її через 2 секунди
+
+setTimeout(timedFunction, 2000);
+
+function timedFunction() {
+  console.log("i am function");
+}
