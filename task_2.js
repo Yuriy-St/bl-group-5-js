82b96f3eb35952e71dce45db717ec140e0022069
Вячеслав <goldensoldat@gmail.com>
2023-06-04 15:03:43 +0300

    Task-2

--- task_2.js
+++ task_2.js
@@ -1,1 +1,13 @@
 //Напишіть функцію, яка виводитиме числа від min до max із затримкою в 1 секунду між кожним значенням
+function printNumbers(min, max) {
+    let currentNumber = min;
+    const intervalId = setInterval(() => {
+
+        console.log(currentNumber);
+        if (currentNumber === max) {
+            clearInterval(intervalId)
+        }
+        currentNumber++;
+    }, 1000)
+}
+printNumbers(1, 10);
