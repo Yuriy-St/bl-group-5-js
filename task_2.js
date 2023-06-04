bd2d3f8278191b60fe8166f8f93cc38d2dfbf274
Volodymyr Kozak <vkozak@reenbit.com>
2023-06-04 13:52:56 +0300

    upload tasks for lesson 5

--- task_2.js
+++ task_2.js
@@ -1,15 +1,1 @@
-// Наступна функція повертає true, якщо параметр age більше 18.
-// В іншому випадку вона запитує підтвердження через confirm і повертає його результат:
-
-const age = prompt('Enter your age')
-
-function isAdult() {
-    if (age >= 18) {
-        return true;
-    } else {
-        const ageAccept = confirm('Підтвердіть свій вік')
-        return ageAccept;
-    }
-}
-
-console.log(isAdult());
+//Напишіть функцію, яка виводитиме числа від min до max із затримкою в 1 секунду між кожним значенням
--- task_3.js
+++ task_3.js
@@ -1,28 +1,5 @@
-//Напиши функцію findSmallerNumber(numbers), яка шукає найменше число в масиві.
-//Додай перевірку що функція отримує масив
-
-const numbers = [2, 5, 35, 56, 12, 24, 7, 1, 80, 3];
-
-function findSmallerNumber(numbers) {
-    if (Array.isArray(numbers)) {
-        console.log("Ok")
-    } else {
-        return console.log("No");
-    }
-    let minVal = numbers[0];
-
-    for (let i = 0; i < numbers.length; i++) {
-        if (minVal >= numbers[i]) {
-            minVal = numbers[i];
-        } 
-    }
-    return minVal;
-};
-console.log(findSmallerNumber(numbers))
-
-
-function findSmallerNumber2(numbers) {
-    console.log(Math.min(...numbers));
-};
-
-const minValue = findSmallerNumber2(numbers);
+// Використовуй prompt та повертай значення звідти.
+// Створи функцію, усередині якої буде проміс.
+// Якщо значення не є числом, відхиляй проміс та повертай "error".
+// Якщо значення парне, вирішуй проміс та повертай "even" через 1 секунду.
+// Якщо значення не парне, вирішуй проміс та повертай "odd" через 2 секунди.
